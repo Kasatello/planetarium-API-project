dec73557d81a92dfbaff0e7f2ac89df0edc9b173
Nazariy <mac@Nazariys-MacBook-Pro.local>
2023-08-11 02:25:50 +0300

    changed authentication field from username to email

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
 from django.db.models import F, Count
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 
 from planetarium.models import (
     PlanetariumDome,
@@ -67,6 +70,23 @@ class AstronomyShowViewSet(viewsets.ModelViewSet):
             return AstronomyShowDetailSerializer
 
         return AstronomyShowSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser],
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to specific movie"""
+        movie = self.get_object()
+        serializer = self.get_serializer(movie, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ShowSessionViewSet(viewsets.ModelViewSet):
